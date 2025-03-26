7699c378de16af081bdc80feab50a26cf1582c4e
Aspect <agentaspect481@gmail.com>
2025-03-26 10:01:09 -0500

    asdg

---
+++ src/lib/Form.svelte
@@ -0,0 +1,44 @@
+<form>
+    <div class="flow">
+        <div class="form-item">
+            <label for="first">First Name</label>
+            <input 
+            type="text"
+            id="first"
+            name="first" 
+            placeholder="Enter your first name..."
+            required
+            /> 
+        </div>
+        <div class="form-item">
+            <label for="first">First Name</label>
+            <input 
+            type="text"
+            id="first"
+            name="first" 
+            placeholder="Enter your first name..."
+            required
+            /> 
+        </div>
+        <div class="form-item">
+            <label for="first">First Name</label>
+            <input 
+            type="text"
+            id="first"
+            name="first" 
+            placeholder="Enter your first name..."
+            required
+            /> 
+        </div>
+        <div class="form-item">
+            <label for="first">First Name</label>
+            <input 
+            type="text"
+            id="first"
+            name="first" 
+            placeholder="Enter your first name..."
+            required
+            /> 
+        </div>
+    </div>
+</form>
