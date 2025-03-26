bcc10c0a17b6dc3f77c00a4260c2199a12931196
Aspect <agentaspect481@gmail.com>
2025-03-26 09:47:52 -0500

    abpout page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,3 +3,75 @@
 
 </script>
  <Title name="About" />
+
+ <section>
+    <div class="outer">
+        <div class="inner flow">
+            <h2>Subheading One</h2>
+            <div class="columns flow">
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam recusandae
+                    magni architecto perspiciatis ullam perferendis commodi! 
+                    Labore et ipsa nemo autem deleniti sed nihil cumque id, aliquam facere voluptate! Accusantium, hic fugiat?</p>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia corporis, numquam voluptas quisquam, 
+                    blanditiis voluptatem praesentium minima vel odio illo reiciendis.</p>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere dolore explicabo hic.</p>
+            </div>
+        </div>
+    
+    </div>
+ </section>
+
+ <section>
+    <div class="outer bg-blue white">
+        <div class="inner flow">
+            <h2>Subheading Two</h2>
+            <div class="columns flow">
+
+                <p class="white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam recusandae
+                    magni architecto perspiciatis ullam perferendis commodi! 
+                    Labore et ipsa nemo autem deleniti sed nihil cumque id, aliquam facere voluptate! Accusantium, hic fugiat?</p>
+
+                <p class="white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia corporis, numquam voluptas quisquam, 
+                    blanditiis voluptatem praesentium minima vel odio illo reiciendis.</p>
+
+                <p class="white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere dolore explicabo hic.</p>
+            </div>
+        </div>
+    
+    </div>
+ </section>
+ 
+ <section>
+    <div class="outer">
+        <div class="inner flow wide-text">
+            <h2>Subheading Three</h2>
+            <img class="float-right" src="chez.jpg" alt="computers on a desk"/>
+           
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam recusandae
+                    magni architecto perspiciatis ullam perferendis commodi! 
+                    Labore et ipsa nemo autem deleniti sed nihil cumque id, aliquam facere voluptate! Accusantium, hic fugiat?</p>
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia corporis, numquam voluptas quisquam, 
+                    blanditiis voluptatem praesentium minima vel odio illo reiciendis.</p>
+
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio animi maxime, totam sunt a quia odit itaque possimus
+                     incidunt doloremque dolore maiores quidem obcaecati eveniet quibusdam enim excepturi culpa eos ipsum ipsa! Suscipit 
+                     quasi magni officiis laboriosam cumque. Blanditiis, assumenda nostrum, pariatur voluptatem explicabo beatae sunt numquam
+                      voluptates maiores commodi excepturi fuga vitae fugiat at optio, qui itaque maxime neque minus quia non deleniti nesciunt 
+                      ullam dolorem? Necessitatibus earum tenetur qui saepe corporis. Non exercitationem voluptatum doloremque quaerat alias 
+                    consectetur saepe maxime facilis facere modi, esse corporis assumenda nesciunt quas aliquam officia labore quam repellendus
+                     autem reprehenderit, suscipit eaque ex?</p>
+            
+        </div>
+    
+    </div>
+ </section>
+ <style>
+    .wide-text  p {
+        max-width: unset;
+    }
+ </style>
