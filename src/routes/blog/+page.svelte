00d3277404a64de45d6b14f0418d10bcf62bd8f7
Aspect <agentaspect481@gmail.com>
2025-04-15 22:11:33 -0500

    im trying to fix my mistakesugggg

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,20 +1,11 @@
 <script>
     import Title from "$lib/Title.svelte";
+	import { json } from "@sveltejs/kit";
 
-    export let data;
+     export let data 
 </script>
 
- <Title name="Products" />
-<!-- {data} -->
-<section>
-    <div class="outer">
-       <div class="inner auto-fit">
-           {#each data.posts as post}
-           <div class="flow">
-               <h2><a href="/Products/{post.id}">{post.title}</a></h2>
-               <p>{post.body}</p>
-           </div>
-           {/each}
-       </div>
-   </div>
-   </section>
+<Title name="Blog" />
+
+
+
