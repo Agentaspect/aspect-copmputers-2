9cee007093cab57ae33d8cef1f0433e67fded829
Aspect <agentaspect481@gmail.com>
2025-05-06 12:26:53 -0500

    final

--- src/routes/blog/[slug]/+page.js
+++ src/routes/blog/[slug]/+page.js
@@ -1,8 +1,9 @@
+// @ts-ignore
 export async function load({fetch, params}) {
     // // const endpoint = 'https://jsonplaceholder.typicode.com/posts/' + params.id
         
     
-    const endpoint = 'https://public-api.wordpress.com/rest/v1.1/sites/repo995752852.wordpress.com/posts/slug:'+
+    const endpoint = 'https://public-api.wordpress.com/rest/v1.1/sites/aspectcomputers3.wordpress.com/posts/slug:'+
     params.slug
     const response = await fetch(endpoint)
     const post = await response.json()
